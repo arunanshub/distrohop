41d23cb43fc1816e232a76a406b1fe9f3bfb5494
Arunanshu Biswas <mydellpc07@gmail.com>
2023-08-08 17:50:32 +0530

    feat(components): add footer component

---
+++ frontend/src/components/TheFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="absolute bottom-0 w-full bg-white p-2">
+    <div
+      class="fw-400 mx-auto flex max-w-prose flex-wrap justify-between gap-4 text-xs text-sky-900 md:max-w-2xl"
+    >
+      <NuxtLink to="/info/imprint/">Imprint</NuxtLink>
+      <NuxtLink to="/info/privacy/">Privacy</NuxtLink>
+      <NuxtLink to="/info/about/">About</NuxtLink>
+      <NuxtLink to="https://github.com/distrochooser/distrochooser">
+        Github
+      </NuxtLink>
+      <NuxtLink to="https://arunanshub.hashnode.dev">
+        A project by @arunanshub
+      </NuxtLink>
+    </div>
+  </footer>
+</template>
