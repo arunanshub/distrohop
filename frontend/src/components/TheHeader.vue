a91edd8dde4bd5f85d32579e019dafbe750852d6
Arunanshu Biswas <mydellpc07@gmail.com>
2023-08-09 13:58:42 +0530

    feat(components): add header and add to layout

---
+++ frontend/src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div class="flex items-center justify-center p-4">
+      <NuxtLink to="/" class="flex items-center">
+        <div>
+          <Icon name="logos:linux-tux" size="8rem" />
+        </div>
+        <span
+          class="font-artifika self-center whitespace-nowrap text-4xl font-semibold"
+        >
+          Distrohop
+        </span>
+      </NuxtLink>
+    </div>
+  </header>
+</template>
