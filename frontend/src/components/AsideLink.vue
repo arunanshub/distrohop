3c1deb5e7bd8877c966fb26ecd259f5427bb969e
Arunanshu Biswas <mydellpc07@gmail.com>
2023-08-10 20:48:20 +0530

    feat(components): add components for aside and intro screen

---
+++ frontend/src/components/AsideLink.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLink :to="to" class="flex items-center justify-center gap-2">
+    <Icon :name="iconName" size="1.5rem" class="h-full md:h-5 md:opacity-40" />
+    <div class="hidden md:block">
+      <slot />
+    </div>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  iconName: string
+  to: string
+}>()
+</script>
---
+++ frontend/src/components/IntroItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li class="flex items-center gap-2">
+    <Icon
+      size="1.2rem"
+      :color="iconColor"
+      :name="iconName"
+      class="min-h-fit min-w-fit opacity-80"
+    />
+    <span><slot /></span>
+  </li>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  iconName: {
+    type: String,
+    required: true,
+  },
+  iconColor: {
+    type: String,
+    default: 'black',
+  },
+})
+</script>
