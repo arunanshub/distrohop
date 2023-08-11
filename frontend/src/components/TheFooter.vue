181002941e09ec5290412d0de8e9c6ce1fcf39b7
Arunanshu Biswas <mydellpc07@gmail.com>
2023-08-12 00:36:57 +0530

    refactor(footer): make footer a block level item

--- frontend/src/components/TheFooter.vue
+++ frontend/src/components/TheFooter.vue
@@ -1,7 +1,7 @@
 <template>
-  <footer class="absolute bottom-0 w-full bg-white p-2">
+  <footer class="mt-8 w-full bg-white px-4 py-2">
     <div
-      class="fw-400 mx-auto flex max-w-prose flex-wrap justify-between gap-4 text-xs text-sky-900 md:max-w-2xl"
+      class="fw-400 mx-auto flex max-w-prose flex-wrap justify-around gap-4 text-xs text-sky-900 md:max-w-2xl md:justify-between"
     >
       <NuxtLink to="/info/imprint/">Imprint</NuxtLink>
       <NuxtLink to="/info/privacy/">Privacy</NuxtLink>
