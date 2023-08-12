31eacff457a50ff1fb8248e5795fc2fbedbc1494
Arunanshu Biswas <mydellpc07@gmail.com>
2023-08-12 16:40:45 +0530

    feat(content): display 404 page from within the content rendering block

--- frontend/src/pages/[...slug].vue
+++ frontend/src/pages/[...slug].vue
@@ -1,24 +1,24 @@
 <template>
-  <Title v-if="data?.title">{{ data.title }}</Title>
   <div
     class="prose mx-auto w-full max-w-screen-lg grow px-8 pb-8 text-justify 2xl:px-0"
-    v-if="data"
   >
-    <LazyContentRenderer :value="data!" v-if="data" />
+    <ContentDoc>
+      <template #not-found>
+        <Title>404</Title>
+        <div
+          class="not-prose mt-[15vh] flex flex-col items-center gap-4 text-center"
+        >
+          <h1 class="text-6xl font-semibold">404</h1>
+          <p class="text-2xl">The page you are looking for does not exist.</p>
+          <NuxtLink to="/">
+            <div
+              class="mt-4 border border-black bg-white p-4 hover:bg-slate-200"
+            >
+              Go Back Home
+            </div>
+          </NuxtLink>
+        </div>
+      </template>
+    </ContentDoc>
   </div>
 </template>
-
-<script setup lang="ts">
-const { path } = useRoute()
-
-const { data, error } = await useAsyncData(`content-${path}`, () => {
-  return queryContent().where({ _path: path }).findOne()
-})
-
-if (error.value || data.value?._empty) {
-  showError({
-    statusCode: 404,
-    statusMessage: `The page you are looking for does not exist`,
-  })
-}
-</script>
