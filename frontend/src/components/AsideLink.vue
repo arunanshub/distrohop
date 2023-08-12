240e6862af553595059093fc732fc88418726d53
Arunanshu Biswas <mydellpc07@gmail.com>
2023-08-12 12:32:44 +0530

    refactor(AsideLink): add padding and hover underline effect

--- frontend/src/components/AsideLink.vue
+++ frontend/src/components/AsideLink.vue
@@ -1,7 +1,10 @@
 <template>
-  <NuxtLink :to="to" class="flex items-center justify-center gap-2">
-    <Icon :name="iconName" size="1.5rem" class="h-full md:h-5 md:opacity-40" />
-    <div class="hidden md:block">
+  <NuxtLink
+    :to="to"
+    class="flex items-center justify-center gap-2 p-2 hover:underline"
+  >
+    <Icon :name="iconName" size="1.5rem" class="h-full lg:h-5 lg:opacity-40" />
+    <div class="hidden lg:block">
       <slot />
     </div>
   </NuxtLink>
