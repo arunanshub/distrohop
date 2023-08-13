4ff6ae3720924e0f3a0994a7e889474d4e63c008
Arunanshu Biswas <mydellpc07@gmail.com>
2023-08-13 16:05:52 +0530

    refactor(layout): keep min-h-screen for compatibility purposes

--- frontend/src/layouts/default.vue
+++ frontend/src/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex min-h-[100dvh] flex-col bg-neutral-100">
+  <div class="flex min-h-[100dvh] min-h-screen flex-col bg-neutral-100">
     <TheHeader />
     <div class="flex grow flex-col">
       <slot />
