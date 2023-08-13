c9180f52712108e2421c20bdb968e5a722af32b4
Arunanshu Biswas <mydellpc07@gmail.com>
2023-08-13 16:19:14 +0530

    fix(layout): use vue scoped style to apply dynamic screen height

--- frontend/src/layouts/default.vue
+++ frontend/src/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex min-h-screen min-h-[100dvh] flex-col bg-neutral-100">
+  <div class="min-h-screen-dynamic flex flex-col bg-neutral-100">
     <TheHeader />
     <div class="flex grow flex-col">
       <slot />
@@ -7,3 +7,10 @@
     <TheFooter />
   </div>
 </template>
+
+<style scoped>
+.min-h-screen-dynamic {
+  /* prettier-ignore */
+  @apply min-h-screen min-h-[100dvh];
+}
+</style>
