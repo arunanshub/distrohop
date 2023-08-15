72214b4fcd40afc8e590311a71fc57216a26ef6b
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2023-08-15 13:51:29 +0530

    feat(error): add error page and use query to find content (#24)

---
+++ frontend/src/error.vue
@@ -0,0 +1,38 @@
+<template>
+  <Title>{{ error.statusCode }}</Title>
+  <div class="min-h-screen-dynamic flex flex-col bg-neutral-100">
+    <TheHeader />
+    <div class="mt-[15vh] flex grow flex-col items-center gap-4 text-center">
+      <h1 class="text-6xl font-semibold">{{ error.statusCode }}</h1>
+      <p class="text-2xl" v-if="error.statusMessage">
+        {{ error.statusMessage }}
+      </p>
+      <button
+        class="mt-4 border border-black bg-white p-4 hover:bg-slate-200"
+        @click="handleError"
+      >
+        Go Back Home
+      </button>
+    </div>
+    <TheFooter />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { NuxtError } from 'nuxt/app'
+
+defineProps<{ error: NuxtError }>()
+
+async function handleError() {
+  await clearError({
+    redirect: '/',
+  })
+}
+</script>
+
+<style scoped>
+.min-h-screen-dynamic {
+  /* prettier-ignore */
+  @apply min-h-screen min-h-[100dvh];
+}
+</style>
