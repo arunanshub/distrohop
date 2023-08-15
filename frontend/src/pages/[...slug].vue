7ce6a5b56c90d35cecec40aa8a845f3d88c6308a
Arunanshu Biswas <mydellpc07@gmail.com>
2023-08-15 13:20:24 +0530

    refactor(content): do not lazy load data

--- frontend/src/pages/[...slug].vue
+++ frontend/src/pages/[...slug].vue
@@ -2,21 +2,18 @@
   <div
     class="prose mx-auto w-full max-w-screen-lg grow hyphens-auto px-8 pb-8 text-justify 2xl:px-0"
   >
-    <LazyContentRenderer :value="data!" v-if="data" />
+    <LazyContentRenderer :value="data" v-if="data" />
   </div>
 </template>
 
 <script setup lang="ts">
 const { path } = useRoute()
 
-const { data, error, pending } = await useLazyAsyncData(
-  `content-${path}`,
-  () => {
-    return queryContent().where({ _path: path }).findOne()
-  }
-)
+const { data, error } = await useAsyncData(`content-${path}`, () => {
+  return queryContent().where({ _path: path }).findOne()
+})
 
-if (error.value || (!pending.value && data.value?._empty)) {
+if (error.value || data.value?._empty) {
   throw createError({
     statusCode: 404,
     statusMessage: 'The page you are looking for does not exist.',
