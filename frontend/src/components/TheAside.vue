ceacecb533e8a13236afa4d265aa6bc80ed5f7b4
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2023-09-02 17:58:00 +0530

    refactor(index): use padding on main div instead of setting it on Aside (#45)

--- frontend/src/components/TheAside.vue
+++ frontend/src/components/TheAside.vue
@@ -1,6 +1,6 @@
 <template>
   <aside
-    class="w-15 lg:w-75 flex select-none flex-col gap-2 pl-2 text-center lg:pl-0"
+    class="w-15 lg:w-75 flex select-none flex-col gap-2 text-center"
   >
     <slot />
   </aside>
