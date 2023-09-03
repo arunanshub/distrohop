79605dc9b6b06b0ce70bcaf1f4d945d85ec26581
Arunanshu Biswas <mydellpc07@gmail.com>
2023-09-03 23:40:21 +0530

    feat(components): add component to render conflicting and important answers

---
+++ frontend/src/components/StarButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="bg-transparent">
+    <Icon
+      :name="isImportant ? 'ph:star-fill' : 'ph:star'"
+      color="#ff7a00"
+      size="1.3rem"
+    />
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{ isImportant?: boolean }>()
+</script>
