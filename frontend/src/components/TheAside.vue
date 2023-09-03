7e0a5be0f17a1f984c9d507b976332dd81859044
Arunanshu Biswas <mydellpc07@gmail.com>
2023-09-03 13:26:11 +0530

    style(prettier): run prettier on codebase

--- frontend/src/components/TheAside.vue
+++ frontend/src/components/TheAside.vue
@@ -1,7 +1,5 @@
 <template>
-  <aside
-    class="w-15 lg:w-75 flex select-none flex-col gap-2 text-center"
-  >
+  <aside class="w-15 lg:w-75 flex select-none flex-col gap-2 text-center">
     <slot />
   </aside>
 </template>
