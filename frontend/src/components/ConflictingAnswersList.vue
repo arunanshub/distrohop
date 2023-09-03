20f1d611fe314c00f47747d718ac4d0a190c3de9
Arunanshu Biswas <mydellpc07@gmail.com>
2023-09-03 23:40:21 +0530

    feat(components): add component to render conflicting and important answers

---
+++ frontend/src/components/ConflictingAnswersList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col bg-yellow-300 p-3">
+    <span class="pb-1">
+      Following answers are in conflict with this answer:
+    </span>
+    <ul class="flex flex-col gap-1">
+      <li
+        class="flex items-center gap-2"
+        v-for="conflictingAnswer in conflictingAnswers"
+        :key="conflictingAnswer"
+      >
+        <Icon
+          name="material-symbols:warning-outline"
+          size="0.8rem"
+          color="red"
+        />
+        <span>"{{ conflictingAnswer }}"</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  conflictingAnswers: string[]
+}>()
+</script>
