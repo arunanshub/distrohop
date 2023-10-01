80e6ab60a54f12a37a84b21cd2725bbe89806585
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2023-10-01 17:32:27 +0530

    refactor(pages): article element should be inside main element (#84)

--- frontend/src/pages/info/[article].vue
+++ frontend/src/pages/info/[article].vue
@@ -1,10 +1,12 @@
 <template>
   <Title v-if="data?.title">{{ data.title }}</Title>
-  <article
-    class="prose prose-p:text-black prose-a:no-underline hover:prose-a:underline mx-auto w-full grow px-8 pb-8 lg:px-0"
-  >
-    <LazyServerContentRenderer :value="data" v-if="data" />
-  </article>
+  <main>
+    <article
+      class="prose prose-p:text-black prose-a:no-underline hover:prose-a:underline mx-auto w-full grow px-8 pb-8 lg:px-0"
+    >
+      <LazyServerContentRenderer :value="data" v-if="data" />
+    </article>
+  </main>
 </template>
 
 <script setup lang="ts">
