cdff52f5190583b05f1207eb47c568f7c7a2687a
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2023-10-07 18:31:52 +0530

    style(components): make aside item children selectable (#91)

--- frontend/src/components/TheAside.vue
+++ frontend/src/components/TheAside.vue
@@ -1,5 +1,5 @@
 <template>
-  <aside class="flex w-16 select-none flex-col gap-2 text-center lg:w-72">
+  <aside class="flex w-16 flex-col gap-2 text-center lg:w-72">
     <slot />
   </aside>
 </template>
