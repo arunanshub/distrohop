29eeeb553519ce4da7bf2f8b518ca53a2ea253db
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2023-10-08 20:27:35 +0530

    refactor(pages): show error message instead of showing the status message (#93)

--- frontend/src/error.vue
+++ frontend/src/error.vue
@@ -1,13 +1,13 @@
 <template>
-  <Title>{{ error.statusCode }}</Title>
   <div class="min-h-screen-dynamic flex flex-col bg-neutral-100">
     <TheHeader />
     <div class="mt-[15vh] flex grow flex-col items-center gap-4 text-center">
       <h1 class="text-6xl font-semibold">{{ error.statusCode }}</h1>
-      <p class="text-2xl" v-if="error.statusMessage">
-        {{ error.statusMessage }}
+      <p class="text-2xl" v-if="error.message && error.message !== ''">
+        {{ error.message }}
       </p>
       <button
+        v-if="$route.path !== '/'"
         class="mt-4 border border-black bg-white p-4 hover:bg-slate-200"
         @click="handleError"
       >
@@ -21,7 +21,9 @@
 <script setup lang="ts">
 import { NuxtError } from 'nuxt/app'
 
-defineProps<{ error: NuxtError }>()
+const props = defineProps<{ error: NuxtError }>()
+
+useServerHead({ title: props.error.statusCode.toString() })
 
 async function handleError() {
   await clearError({
