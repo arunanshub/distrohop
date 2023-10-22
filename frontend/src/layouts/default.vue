b20b8a27791741311781b00ab0a1e4379e17dab0
Arunanshu Biswas <mydellpc07@gmail.com>
2023-10-22 22:49:48 +0530

    feat(layout): experiment with nuxt loading indicator

--- frontend/src/layouts/default.vue
+++ frontend/src/layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="min-h-screen-dynamic flex flex-col bg-neutral-100">
+    <NuxtLoadingIndicator class="w-full h-16" />
     <TheHeader />
     <div class="flex grow flex-col">
       <slot />
