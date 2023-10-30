a0cafb8f77822048126ecb39b585e62627849668
Arunanshu Biswas <mydellpc07@gmail.com>
2023-10-30 21:03:39 +0530

    feat(composables): add composable to calculate conflicting answers

---
+++ frontend/src/composables/useConflictingAnswers.ts
@@ -0,0 +1,16 @@
+import type { Answer } from '~/types'
+
+export default (
+  collectedAnswers: Ref<Set<string>>,
+  currentAnswers: Answer[]
+) => {
+  function getConflictingAnswers(answerId: string) {
+    const answer = currentAnswers.find((ans) => ans.msgid === answerId)
+    if (!answer) {
+      return
+    }
+    const blockedByIds = answer.blockedBy.map((ans) => ans.msgid)
+    return blockedByIds.filter((ans) => collectedAnswers.value.has(ans))
+  }
+  return { getConflictingAnswers }
+}
