5796c1ec825981f83600adbacbc60295fa726f5d
Arunanshu Biswas <mydellpc07@gmail.com>
2023-11-04 03:22:17 +0530

    fix(prisma): do not use prisma accelerate

--- frontend/src/server/middleware/0.prisma.ts
+++ frontend/src/server/middleware/0.prisma.ts
@@ -1,10 +1,9 @@
 import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
 
 type ExtendedPrismaClient = ReturnType<typeof getExtendedClient>
 
 function getExtendedClient() {
-  return new PrismaClient().$extends(withAccelerate())
+  return new PrismaClient()
 }
 
 let prisma: ExtendedPrismaClient
