6545cfc23b75c2a4d8bb3fa7cc39bcee1c4a18e3
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2023-11-22 17:42:48 +0530

    feat(prisma): add `Distribution` model and seeding data (#137)

--- prisma/migrations/20231122104738_init/migration.sql
+++ prisma/migrations/20231122104738_init/migration.sql
@@ -1,6 +1,3 @@
--- CreateExtension
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
 -- CreateTable
 CREATE TABLE "Section" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
@@ -33,16 +30,41 @@ CREATE TABLE "Answer" (
 );
 
 -- CreateTable
+CREATE TABLE "Distribution" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" VARCHAR(200) NOT NULL,
+    "identifier" VARCHAR(200) NOT NULL,
+    "fgColor" VARCHAR(100),
+    "bgColor" VARCHAR(100),
+    "logo" TEXT,
+    "url" TEXT,
+
+    CONSTRAINT "Distribution_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Result" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "ip" INET,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "ip" INET NOT NULL,
 
     CONSTRAINT "Result_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "_BlockedAnswer" (
+    "A" UUID NOT NULL,
+    "B" UUID NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_SelectedAnswers" (
+    "A" UUID NOT NULL,
+    "B" UUID NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_ImportantAnswers" (
     "A" UUID NOT NULL,
     "B" UUID NOT NULL
 );
@@ -60,10 +82,25 @@ CREATE UNIQUE INDEX "Question_sectionId_key" ON "Question"("sectionId");
 CREATE UNIQUE INDEX "Answer_msgid_key" ON "Answer"("msgid");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Distribution_identifier_key" ON "Distribution"("identifier");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "_BlockedAnswer_AB_unique" ON "_BlockedAnswer"("A", "B");
 
 -- CreateIndex
 CREATE INDEX "_BlockedAnswer_B_index" ON "_BlockedAnswer"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_SelectedAnswers_AB_unique" ON "_SelectedAnswers"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_SelectedAnswers_B_index" ON "_SelectedAnswers"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ImportantAnswers_AB_unique" ON "_ImportantAnswers"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ImportantAnswers_B_index" ON "_ImportantAnswers"("B");
 
 -- AddForeignKey
 ALTER TABLE "Question" ADD CONSTRAINT "Question_sectionId_fkey" FOREIGN KEY ("sectionId") REFERENCES "Section"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -76,3 +113,15 @@ ALTER TABLE "_BlockedAnswer" ADD CONSTRAINT "_BlockedAnswer_A_fkey" FOREIGN KEY 
 
 -- AddForeignKey
 ALTER TABLE "_BlockedAnswer" ADD CONSTRAINT "_BlockedAnswer_B_fkey" FOREIGN KEY ("B") REFERENCES "Answer"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_SelectedAnswers" ADD CONSTRAINT "_SelectedAnswers_A_fkey" FOREIGN KEY ("A") REFERENCES "Answer"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_SelectedAnswers" ADD CONSTRAINT "_SelectedAnswers_B_fkey" FOREIGN KEY ("B") REFERENCES "Result"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ImportantAnswers" ADD CONSTRAINT "_ImportantAnswers_A_fkey" FOREIGN KEY ("A") REFERENCES "Answer"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ImportantAnswers" ADD CONSTRAINT "_ImportantAnswers_B_fkey" FOREIGN KEY ("B") REFERENCES "Result"("id") ON DELETE CASCADE ON UPDATE CASCADE;
