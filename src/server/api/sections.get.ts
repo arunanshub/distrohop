06886afe716d9f0fede0850ed76948e449007412
Arunanshu Biswas <mydellpc07@gmail.com>
2024-02-08 15:44:52 +0530

    feat(api/sections): change caching params and use crud ops

--- src/server/api/sections.get.ts
+++ src/server/api/sections.get.ts
@@ -1,11 +1,8 @@
-import { SectionWithQuestionId, sectionWithQuestionId } from '~/types'
+import { getSectionsWithoutId } from '@/server/crud/section'
 
 export default defineCachedEventHandler(
-  async (event): Promise<SectionWithQuestionId[]> => {
-    return event.context.prisma.section.findMany({
-      ...sectionWithQuestionId,
-      cacheStrategy: { swr: 60, ttl: 60 },
-    })
+  async (event) => {
+    return await getSectionsWithoutId(event.context.db)
   },
-  { swr: true, staleMaxAge: 300 }
+  { swr: true, staleMaxAge: 3600, maxAge: 3600 },
 )
