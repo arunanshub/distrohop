9c9289a2c58dc3b61528dcfde037ad6c9a12780b
Arunanshu Biswas <mydellpc07@gmail.com>
2024-02-08 15:43:15 +0530

    style(db): apply biomejs

--- src/server/database/db.ts
+++ src/server/database/db.ts
@@ -1,8 +1,8 @@
-import { drizzle } from 'drizzle-orm/planetscale-serverless'
 import { connect } from '@planetscale/database'
+import { drizzle } from 'drizzle-orm/planetscale-serverless'
 import * as answers from './schema/answers'
-import * as questions from './schema/questions'
 import * as distributions from './schema/distributions'
+import * as questions from './schema/questions'
 import * as results from './schema/results'
 import * as sections from './schema/sections'
 
