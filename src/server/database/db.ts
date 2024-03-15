7ceea8ab5bd90e086d1b20d13931dc29b5ef4d42
Arunanshu Biswas <mydellpc07@gmail.com>
2024-03-15 16:18:38 +0530

    build(deps-dev): update dependencies

--- src/server/database/db.ts
+++ src/server/database/db.ts
@@ -1,14 +1,12 @@
-import { Client } from '@planetscale/database'
-import { drizzle } from 'drizzle-orm/planetscale-serverless'
+import { drizzle } from 'drizzle-orm/d1'
 import * as answers from './schema/answers'
 import * as distributions from './schema/distributions'
 import * as questions from './schema/questions'
 import * as results from './schema/results'
 import * as sections from './schema/sections'
 
-export function getDb(url: string) {
-  const connection = new Client({ url })
-  return drizzle(connection, {
+export function getDb(db: D1Database) {
+  return drizzle(db, {
     schema: {
       ...questions,
       ...answers,
