778620548d8f8e629be50388b9aff0330610ce76
Arunanshu Biswas <mydellpc07@gmail.com>
2024-06-30 18:39:33 +0530

    build(nuxt): move to minimal nuxt config

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -3,12 +3,13 @@
     <div
       class="mx-auto flex w-full select-none items-center justify-center p-4"
     >
-      <NuxtLink to="/" class="flex items-center">
+      <NuxtLink to="/" class="flex items-center gap-3">
         <Icon
           name="logos:linux-tux"
           size="8rem"
           aria-label="Distrohop logo"
           class="h-14 w-20 lg:h-40 lg:w-full"
+          mode="svg"
         />
         <span
           class="font-artifika self-center whitespace-nowrap text-2xl font-semibold lg:text-4xl"
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen-dynamic flex flex-col bg-neutral-100">
+  <div class="min-h-dvh flex flex-col bg-neutral-100">
     <NuxtLoadingIndicator color="#3c3c3c" />
     <TheHeader />
     <div class="flex grow flex-col">
