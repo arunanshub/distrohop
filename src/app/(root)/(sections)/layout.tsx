eaf8b1b38e50014211f9461a10f8bca55afccb39
Arunanshu Biswas <mydellpc07@gmail.com>
2025-03-23 11:00:34 +0530

    feat: add caching of server responses

--- src/app/(root)/(sections)/layout.tsx
+++ src/app/(root)/(sections)/layout.tsx
@@ -2,13 +2,16 @@ import { getSections } from "./actions"
 import { CounterStoreProvider } from "@/components/providers/counter-store-provider"
 import { AnswerStoreProvider } from "@/components/providers/answer-store-provider"
 import ClientLayout from "./client-layout"
+import { unstable_cache as cache } from "next/cache"
+
+const getCachedSections = cache(getSections, ["sections"])
 
 export default async function AppLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const sections = await getSections()
+  const sections = await getCachedSections()
 
   return (
     <CounterStoreProvider>
--- src/app/(root)/(sections)/section/[sectionId]/page.tsx
+++ src/app/(root)/(sections)/section/[sectionId]/page.tsx
@@ -1,5 +1,8 @@
 import { getQuestion } from "./actions"
 import Client from "./client"
+import { unstable_cache as cache } from "next/cache"
+
+const getCachedQuestion = cache(getQuestion, ["question"])
 
 export default async function Page({
   params,
@@ -7,6 +10,6 @@ export default async function Page({
   params: Promise<{ sectionId: string }>
 }) {
   const { sectionId } = await params
-  const question = await getQuestion(sectionId)
+  const question = await getCachedQuestion(sectionId)
   return <Client question={question} />
 }
