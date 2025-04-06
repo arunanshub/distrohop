e9e4e98e446f448932b2425d026e64f44b97be56
Arunanshu Biswas <mydellpc07@gmail.com>
2025-04-06 16:41:15 +0530

    feat: use postgres error codes

--- src/app/(root)/(sections)/section/[sectionId]/actions.ts
+++ src/app/(root)/(sections)/section/[sectionId]/actions.ts
@@ -1,7 +1,16 @@
 import { getQuestionBySection } from "@/lib/db/questions"
+import { notFound } from "next/navigation"
+import { DatabaseError } from "pg"
 
 export async function getQuestion(sectionId: string) {
-  return await getQuestionBySection(sectionId)
+  try {
+    return await getQuestionBySection(sectionId)
+  } catch (error) {
+    if (error instanceof DatabaseError && error.code === "P0002") {
+      return notFound()
+    }
+    throw error
+  }
 }
 
 export type Question = Awaited<ReturnType<typeof getQuestion>>
