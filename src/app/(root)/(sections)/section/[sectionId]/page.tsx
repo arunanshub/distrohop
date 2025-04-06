c33c53902117a2583b7a899688cb6b88da4c7d82
Arunanshu Biswas <mydellpc07@gmail.com>
2025-04-06 17:48:25 +0530

    feat: use notfound

--- src/app/(root)/(sections)/section/[sectionId]/page.tsx
+++ src/app/(root)/(sections)/section/[sectionId]/page.tsx
@@ -1,8 +1,9 @@
 import { getQuestion } from "./actions"
 import Client from "./client"
-// import { unstable_cache as cache } from "next/cache"
+import { unstable_cache as cache } from "next/cache"
+import { notFound } from "next/navigation"
 
-// const getCachedQuestion = cache(getQuestion, ["question"])
+const getCachedQuestion = cache(getQuestion, ["question"])
 
 export default async function Page({
   params,
@@ -10,16 +11,11 @@ export default async function Page({
   params: Promise<{ sectionId: string }>
 }) {
   const { sectionId } = await params
-  const question = await getQuestion(sectionId)
+
+  const question = await getCachedQuestion(sectionId)
   if (!question) {
-    return (
-      <div className="flex h-full flex-col items-center justify-center">
-        <h1 className="text-2xl font-bold">Question not found</h1>
-        <p className="text-muted-foreground text-sm">
-          The question you are looking for does not exist.
-        </p>
-      </div>
-    )
+    return notFound()
   }
+
   return <Client question={question} sectionId={sectionId} />
 }
