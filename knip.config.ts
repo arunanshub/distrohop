5cf04f2c77bfc5fff6fa4012c49b5db4a9d00d04
Arunanshu Biswas <mydellpc07@gmail.com>
2025-05-16 09:24:55 +0530

    add shadcn components

--- knip.config.ts
+++ knip.config.ts
@@ -1,5 +1,11 @@
 import { KnipConfig } from "knip"
 
 export default {
-  ignoreDependencies: [/.*eslint.*/, "tailwindcss", "postcss"],
+  ignoreDependencies: [
+    /.*eslint.*/,
+    "tailwindcss",
+    "postcss",
+    "tw-animate-css",
+  ],
+  ignore: ["src/components/ui/*"],
 } satisfies KnipConfig
