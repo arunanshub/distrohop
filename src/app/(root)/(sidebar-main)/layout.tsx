db00ed1f1fa768d66a6670a18ca1f41511737432
Arunanshu Biswas <mydellpc07@gmail.com>
2025-05-17 14:44:16 +0530

    make layout dynamic

--- src/app/(root)/(sidebar-main)/layout.tsx
+++ src/app/(root)/(sidebar-main)/layout.tsx
@@ -1,5 +1,8 @@
 import AppSidebar from "@/components/app-sidebar"
 
+// Need to make the page dynamic since app-sidebar loads data from the server.
+export const dynamic = "force-dynamic"
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="@container/layout mx-auto flex w-full max-w-6xl border-x">
