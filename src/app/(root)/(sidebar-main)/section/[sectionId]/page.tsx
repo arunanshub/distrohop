6d0677345cd0bc7fcd4d054145eac53aa8fa8cda
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2025-06-07 00:52:04 +0530

    Update page.tsx
    Signed-off-by: Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>

--- src/app/(root)/(sidebar-main)/section/[sectionId]/page.tsx
+++ src/app/(root)/(sidebar-main)/section/[sectionId]/page.tsx
@@ -5,7 +5,6 @@ import { Metadata } from "next"
 import { unstable_cache as cache } from "next/cache"
 
 export const metadata: Metadata = {
-  title: "Section",
   alternates: {
     canonical: "/",
   },
