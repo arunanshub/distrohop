855b9879c00b98ec06da442971c34576d560ec52
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2025-06-07 11:18:19 +0530

    feat: submit answers (#412)
    * feat: submit answers
    
    * make todo warning
    
    * prefetch results page
    
    * remove the bogus todo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from "path"
 import { fileURLToPath } from "url"
 import { FlatCompat } from "@eslint/eslintrc"
 import query from "@tanstack/eslint-plugin-query"
+import sonar from "eslint-plugin-sonarjs"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -13,6 +14,13 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   ...query.configs["flat/recommended"],
+  {
+    ...sonar.configs.recommended,
+    rules: {
+      ...sonar.configs.recommended.rules,
+      "sonarjs/todo-tag": "warn",
+    },
+  },
 ]
 
 export default eslintConfig
--- src/components/app-sidebar/results-button.tsx
+++ src/components/app-sidebar/results-button.tsx
@@ -1,29 +1,40 @@
 "use client"
 
 import { BarChart } from "lucide-react"
-import { Button } from "../ui/button"
-import { useRouter } from "next/navigation"
+import { Button } from "@/components/ui/button"
 import { toast } from "sonner"
 import { useMutation } from "@tanstack/react-query"
+import { useAnswerStore } from "@/providers/answer-store-provider"
+import { submitAnswers as submitAnswersAction } from "@/actions/answers"
+import { useRouter } from "next/navigation"
 
 export default function ResultsButton() {
+  const selectedAnswers = useAnswerStore((state) => state.selectedAnswers)
+  const importantAnswers = useAnswerStore((state) => state.importantAnswers)
   const router = useRouter()
 
   const { mutate: submitAnswers } = useMutation({
-    mutationFn: async () => {
-      // TODO: use a mutation to submit the answers and get the results.
-      await new Promise((resolve) => setTimeout(resolve, 1000))
-    },
+    mutationFn: async () =>
+      await submitAnswersAction({ selectedAnswers, importantAnswers }),
   })
 
   function handleClick() {
-    // TODO: use a mutation to submit the answers and get the results.
-    submitAnswers()
-    if (true) {
-      toast.warning("Not implemented yet")
-      return
-    }
-    router.push("/results")
+    const id = toast.loading("Submitting answers...")
+    // Prefetch the results page to improve performance
+    router.prefetch("/results")
+
+    submitAnswers(undefined, {
+      onError(error) {
+        toast.error("Failed to submit answers", {
+          id,
+          description: error instanceof Error ? error.message : "Unknown error",
+        })
+      },
+      onSuccess() {
+        toast.success("Answers submitted successfully", { id })
+        router.push("/results")
+      },
+    })
   }
 
   return (
