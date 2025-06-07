ebaf76c4e9ad2309fe000b300ab118e78c03c578
Arunanshu Biswas <mydellpc07@gmail.com>
2025-06-07 10:32:07 +0530

    feat: submit answers

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from "path"
 import { fileURLToPath } from "url"
 import { FlatCompat } from "@eslint/eslintrc"
 import query from "@tanstack/eslint-plugin-query"
+import sonar from "eslint-plugin-sonarjs"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -10,9 +11,17 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
+// TODO:
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   ...query.configs["flat/recommended"],
+  {
+    ...sonar.configs.recommended,
+    rules: {
+      ...sonar.configs.recommended.rules,
+      "sonarjs/todo-tag": "off",
+    },
+  },
 ]
 
 export default eslintConfig
---
+++ src/actions/answers.ts
@@ -0,0 +1,17 @@
+"use server"
+
+export async function submitAnswers({
+  selectedAnswers,
+  importantAnswers,
+}: {
+  selectedAnswers: Set<string>
+  importantAnswers: Set<string>
+}) {
+  // do some computation
+  console.log("Submitting answers:", {
+    selectedAnswers: Array.from(selectedAnswers),
+    importantAnswers: Array.from(importantAnswers),
+  })
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+  return { answer: 42 }
+}
