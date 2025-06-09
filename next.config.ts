7407aa4b1332703435a560edc3ca64e872af742f
Arunanshu Biswas <mydellpc07@gmail.com>
2025-06-09 17:44:23 +0530

    disable memory optimizations

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   experimental: {
-    webpackMemoryOptimizations: true,
+    // webpackMemoryOptimizations: true,
     inlineCss: true,
   },
 
