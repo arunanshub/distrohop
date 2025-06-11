13cc5e92acdb8c27aeb26898380f967ba0214bd7
Arunanshu Biswas <mydellpc07@gmail.com>
2025-06-12 02:19:48 +0530

    custom component for rendering image based checkbox

--- src/app/(root)/(sidebar-main)/section/[sectionId]/components/answer-checkbox-group.tsx
+++ src/app/(root)/(sidebar-main)/section/[sectionId]/components/answer-checkbox-group.tsx
@@ -8,6 +8,8 @@ import { useAnswerStatus, useSelectedAnswer } from "@/hooks/answers"
 import { useCurrentSectionStatus } from "@/hooks/sections"
 import { useEffect } from "react"
 import MarkAsImportantButton from "./mark-as-important-button"
+import { cn } from "@/lib/utils"
+import Image from "next/image"
 
 export default function AnswerCheckboxGroup({
   question,
@@ -28,16 +30,79 @@ export default function AnswerCheckboxGroup({
     }
   }, [selectedAnswer, markSectionAsUnvisited, markSectionAsVisited])
 
+  const questionHasMediaAnswers = question.answers.every(
+    (answer) => answer.mediaSourcePath !== null,
+  )
+
+  if (questionHasMediaAnswers) {
+    return (
+      <ul className="flex flex-col items-center gap-4 @2xl/page:flex-row @2xl/page:justify-center">
+        {question.answers.map((answer) => (
+          <li key={answer.msgid} className="flex items-start gap-2">
+            <AnswerCheckboxImage answer={answer} />
+            <ConflictingAnswersList answer={answer} />
+          </li>
+        ))}
+      </ul>
+    )
+  }
+
   return (
-    <div className="flex flex-col gap-4">
+    <ul className="flex flex-col gap-4">
       {question.answers.map((answer) => (
-        <div key={answer.msgid}>
+        <li key={answer.msgid}>
           <div className="flex flex-col gap-2">
             <AnswerCheckbox answer={answer} />
             <ConflictingAnswersList answer={answer} />
           </div>
-        </div>
+        </li>
       ))}
+    </ul>
+  )
+}
+
+/** Component for rendering an answer with a checkbox and an image.
+ */
+function AnswerCheckboxImage({ answer }: { answer: Answer }) {
+  const addAnswer = useAnswerStore((store) => store.addAnswer)
+  const removeAnswer = useAnswerStore((store) => store.removeAnswer)
+
+  const { isAnswerSelected } = useAnswerStatus(answer.msgid)
+
+  return (
+    <div>
+      <Label className="flex flex-col gap-1 text-base" htmlFor={answer.msgid}>
+        <Image
+          src={answer.mediaSourcePath!}
+          alt={answer.msgid}
+          width={400}
+          height={400}
+          priority
+          className={cn(
+            "h-auto w-auto border-4",
+            isAnswerSelected ? "border-blue-500" : "border-transparent",
+          )}
+        />
+
+        <Checkbox
+          id={answer.msgid}
+          checked={isAnswerSelected}
+          hidden
+          onCheckedChange={(val) => {
+            if (val === true) {
+              addAnswer(answer.msgid)
+            } else {
+              removeAnswer(answer.msgid)
+            }
+          }}
+        />
+
+        <span className="text-base">{answer.msgid}</span>
+
+        <div className="h-5">
+          {isAnswerSelected && <MarkAsImportantButton answer={answer} />}
+        </div>
+      </Label>
     </div>
   )
 }
@@ -49,7 +114,12 @@ function AnswerCheckbox({ answer }: { answer: Answer }) {
   const { isAnswerSelected } = useAnswerStatus(answer.msgid)
 
   return (
-    <div className="flex items-center gap-2">
+    <div
+      className={cn(
+        "flex items-center gap-2",
+        answer.mediaSourcePath && "w-full flex-col",
+      )}
+    >
       <Checkbox
         id={answer.msgid}
         className="size-6"
