287b981161af1685f995b02d0e3e9cb8ffde55c8
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2025-06-14 13:54:26 +0530

    feat(conflicting-ans-list): add link to section (#445)
    * feat: link to section that contains the answer
    
    * fix drizzle config to avoid bug
    
    * add indices to speed up queries

--- src/server/db/schema/questions.ts
+++ src/server/db/schema/questions.ts
@@ -1,25 +1,29 @@
 import { relations } from "drizzle-orm/relations"
-import { boolean, text } from "drizzle-orm/pg-core"
+import { boolean, text, index } from "drizzle-orm/pg-core"
 import { answers } from "./answers"
 import { sections } from "./sections"
 import { ulid } from "@std/ulid"
 import { createTable } from "./utils"
 
-export const questions = createTable("questions", {
-  id: text("id")
-    .$defaultFn(() => ulid())
-    .primaryKey()
-    .notNull(),
-  msgid: text("msgid").unique().notNull(),
-  isMultipleChoice: boolean("is_multiple_choice").default(false),
-  isMediaQuestion: boolean("is_media_question").default(false),
-  additionalInfo: text("additional_info"),
-  sectionId: text("section_id")
-    .notNull()
-    .references(() => sections.id, {
-      onDelete: "cascade",
-    }),
-})
+export const questions = createTable(
+  "questions",
+  {
+    id: text("id")
+      .$defaultFn(() => ulid())
+      .primaryKey()
+      .notNull(),
+    msgid: text("msgid").unique().notNull(),
+    isMultipleChoice: boolean("is_multiple_choice").default(false),
+    isMediaQuestion: boolean("is_media_question").default(false),
+    additionalInfo: text("additional_info"),
+    sectionId: text("section_id")
+      .notNull()
+      .references(() => sections.id, {
+        onDelete: "cascade",
+      }),
+  },
+  (t) => [index().on(t.sectionId)],
+)
 
 export const questionsRelations = relations(questions, ({ many, one }) => ({
   answers: many(answers),
