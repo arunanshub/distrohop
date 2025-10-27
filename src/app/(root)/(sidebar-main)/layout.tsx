6a33527dfe4517c5bfec5ef68dceea52d58cdc2c
Arunanshu Biswas <mydellpc07@gmail.com>
2025-10-27 10:44:28 +0530

    fetch sections in sidebar

--- src/app/(root)/(sidebar-main)/layout.tsx
+++ src/app/(root)/(sidebar-main)/layout.tsx
@@ -1,4 +1,3 @@
-import { getSections } from "@/actions/sections"
 import AppSidebar from "@/components/app-sidebar"
 import AnswerStoreProvider from "@/providers/answer-store-provider"
 import SectionStoreProvider from "@/providers/section-store-provider"
@@ -13,13 +12,11 @@ export default async function Layout({
 }: {
   children: React.ReactNode
 }) {
-  const sectionsPromise = getSections()
-
   return (
     <SectionStoreProvider>
       <AnswerStoreProvider>
         <div className="@container/layout mx-auto flex w-full max-w-6xl border-x">
-          <AppSidebar sectionsPromise={sectionsPromise} />
+          <AppSidebar />
           <div className="@container/page flex-1 p-4">{children}</div>
         </div>
       </AnswerStoreProvider>
