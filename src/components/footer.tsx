ae8ce195550e4cf3318299a1c7d6a7eea85cd996
Arunanshu Biswas <mydellpc07@gmail.com>
2025-11-24 08:29:32 +0000

    feat: use bun and preload pages

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,10 +5,14 @@ export default function Footer() {
   return (
     <footer className="w-full border-t p-4 text-sm tracking-tight">
       <div className="mx-auto flex h-4 max-w-6xl items-center justify-evenly gap-2 text-sm md:gap-0">
-        <Link href="/imprint">Imprint</Link>
+        <Link href="/imprint" prefetch>
+          Imprint
+        </Link>
         <Separator orientation="vertical" decorative />
 
-        <Link href="/privacy">Privacy</Link>
+        <Link href="/privacy" prefetch>
+          Privacy
+        </Link>
         <Separator orientation="vertical" decorative />
 
         <Link href="/about">About</Link>
@@ -17,7 +21,13 @@ export default function Footer() {
         <Link href="#">A project by @arunanshub</Link>
         <Separator orientation="vertical" decorative />
 
-        <Link href="#">Github</Link>
+        <a
+          href="https://github.com/arunanshub/distrohop"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
       </div>
     </footer>
   )
